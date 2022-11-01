a24841632e4a3938ffb3c0475dbd8db7489cda93
Facundo Caamaño <facundoadriancaamano@outlook.com>
2022-11-01 17:54:42 -0300

    agregando ordenes de compras al firestore

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -1,17 +1,45 @@
-import { useContext } from "react"
+import { useContext,useState } from "react"
 import { CartContext } from "../../context/CartContext"
+import { db } from "../../utils/firebase"
+import { collection, addDoc } from "firebase/firestore"
+
 
 export const CartContainer =()=>{
     const value =useContext(CartContext)
-    const {productosCarrito,getTotalPrice, getTotalProducts,removeItem}=value
-    console.log(productosCarrito)
+    const {productosCarrito,getTotalPrice, getTotalProducts,removeItem}=value;
+    const [compraId,setCompraId]=useState('')
+
+    
+    const sendOrden=(evt)=>{
+        evt.preventDefault();
+        const compra ={
+            buyer:{
+                name: evt.target[0].value,
+                phone: evt.target[1].value,
+                email: evt.target[2].value,
+                direccion:evt.target[3].value
+            },
+            items: productosCarrito,
+            total: getTotalPrice()
+        }
+        //crear referencia
+        const queryRef=collection(db,"orders")
+        //informacion que vamos a agregar
+        addDoc(queryRef, compra).then((resultado)=>{
+            console.log(resultado)
+            setCompraId(resultado.id)
+        })
+
+
+    }
+
     return(
     <div>
         <p>pagina de carrito</p>
         <div>
             {
                 productosCarrito.map((producto)=>(
-                    <div>
+                    <div key={producto.id}>
                         <h3>id:{producto.id}</h3>
                         <img style={{width:300}} src={producto.img} alt="foto produucto"/>
                         <h2>marca y modelo :{producto.marca} {producto.modelo}</h2>
@@ -23,6 +51,22 @@ export const CartContainer =()=>{
             }
             <p><strong>Precio total:</strong>{getTotalPrice()}</p>
             <p><strong>Cantidad de productos:</strong>{getTotalProducts()}</p>
+            <form onSubmit={sendOrden}>
+                <label>Nombre</label>
+                <input type="text" placeholder="Nombre" />
+                <label>Telefono</label>
+                <input type="number" placeholder="Numero de telefono" />
+                <label>Correo</label>
+                <input type="email" placeholder="Ingrese su Email" />
+                <label>Direccion</label>
+                <input type="direccion" placeholder="Direccion de envio" />
+                <button type="submit">Enviar</button>
+
+            </form>
+            <div>
+                {compraId && <h3>el id de su compra es {compraId}</h3>}
+                       
+            </div>
         </div>
     </div>
     )
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,8 +3,14 @@ import {Link} from 'react-router-dom'
 export const Item =({info})=>{
     return(
         <Link to={`/item/${info.id}`} className="contenedor__card">
-            <img className="producto__foto" src={info.img} alt="" />
-            <p className="porducto__nombre">{info.marca} {info.modelo}</p>
+            <div className="contenedor__producto--foto">
+                <img className="producto__foto" src={info.img} alt="" />
+            </div>
+            <div className="contenedor__producto--info">
+                <p className="porducto__nombre">{info.marca} </p>
+                <p>{info.modelo}</p>
+                <p>{info.price}</p>
+            </div>
         </Link>
     )
     
